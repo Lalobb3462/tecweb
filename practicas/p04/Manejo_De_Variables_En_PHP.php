834d9bdcbdfa6b07d2a0197627103ed41cc895b9
LaloBallinas <lalobb346@gmail.com>
2025-09-03 01:02:07 -0600

    Pregunta 2 añadida a Manejo_De_Variables_En_PHP_.php

--- practicas/p04/Manejo_De_Variables_En_PHP.php
+++ practicas/p04/Manejo_De_Variables_En_PHP.php
@@ -4,11 +4,11 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title> Práctica 4 </title>
+
 </head>
 <body>
     <h1>Manejo de variables en PHP</h1>
-    <h2>Inciso 1. Determina cuál de las siguientes variables son válidas y explica por qué: </h2><br>
-
+    <h2>Pregunta 1. Determina cuál de las siguientes variables son válidas y explica por qué: </h2><br>
     <?php
         echo '$_myvar es válida porque cumple con los estándares establecidos de PHP<br>';
         echo '$_7var es válida porque cumple con los estándares establecidos de PHP<br>';
@@ -18,5 +18,35 @@
         echo '$_element1 porque cumple con los estándares establecidos de PHP<br>';
         echo '$house*5 no es válida ya que * es un carácter especial<br>';
     ?>
+    <br>
+    <h2>Pregunta 2. Proporcionar los valores de $a, $b, $c como sigue:</h2>
+    <?php
+        echo "<p>\$a = \"ManejadorSQL\"<br>
+                \$b = 'MySQL'<br>
+                \$c = &\$a;        
+        </p>";
+        
+        echo "<h3>a. Mostrar el contenido de cada variable </h3>";
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        echo "Variable \$a:  $a <br>";
+        echo "Variable \$b:  $b <br>";
+        echo "Variable \$c:  $c <br>";
+        echo "<h3>b. Agregar al código actual las siguientes asignaciones: </h3>";
+        $a = "PHP server";
+        $b = &$a;
+        echo "<p>\$a = \"PHP server\"<br> 
+                \$b = &\$a;        
+        </p>";
+        echo "<h3>c. Volver a mostrar el contenido de cada uno </h3>";
+        echo "Variable \$a:  $a <br>";
+        echo "Variable \$b:  $b <br>";
+        echo "Variable \$c:  $c <br>";
+        echo "<h3>d. Describir que sucedió en el segundo bloque de asignaciones</h3>";
+        echo "En la segunda asignación la variable b y c obtuvieron el contenido actual de la variable a, el cual era \"PHP server\" en el momento de su obtención."
+    ?>
+
+
 </body>
 </html>
