a8ba76d6b1bfe8c39c45635787cda2101c120cf2
LaloBallinas <lalobb346@gmail.com>
2025-09-11 20:16:55 -0600

    Formulario.html añadido a la práctica

--- practicas/p04/Manejo_De_Variables_En_PHP.php
+++ practicas/p04/Manejo_De_Variables_En_PHP.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
-“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -149,7 +149,7 @@
         echo "\$b = \"TRUE\"<br>";
         echo "\$c = \"FALSE\"<br>";
         echo "\$d = (\$a OR \$b)<br>";
-        echo "\$e = (\$a AMD \$c)<br>";
+        echo "\$e = (\$a AND \$c)<br>";
         echo "\$f = (\$a XOR \$b)<br>";
         $a = "0";
         $b = "TRUE";
