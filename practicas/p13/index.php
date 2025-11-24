2da1208cd2cb21339596db42a698d9d49ed4cad3
LaloBallinas <lalobb346@gmail.com>
2025-11-24 14:46:05 -0600

    P13 | index.php y .htaccess configurados para el navegador

---
+++ practicas/p13/index.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/', function($request, $response, $args ) {
+    $response->write("Hola Mundo Slim!!!!");
+    return $response;
+});
+
+$app->get("/hola[/{nombre}]", function ($request, $response, $args){
+    $response->write("Hola, " .$args["nombre"]);
+    return $response;
+});
+
+$app->run();
+
+?>
