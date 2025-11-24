2a07c96cb1289ffa70500e5195799f7a2bc09b50
LaloBallinas <lalobb346@gmail.com>
2025-11-24 15:03:23 -0600

    P13 | Método POST y formulario agregados

--- practicas/p13/index.php
+++ practicas/p13/index.php
@@ -13,6 +13,16 @@ $app->get("/hola[/{nombre}]", function ($request, $response, $args){
     return $response;
 });
 
+
+$app->post("/pruebapost", function($request, $response, $args){
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"];
+    $val2 = $reqPost["val2"];
+
+    $response->write("Valores: " .$val1 . " " . $val2);
+    return $response;
+});
+
 $app->run();
 
 ?>
